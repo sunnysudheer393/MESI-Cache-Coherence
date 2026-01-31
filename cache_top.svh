6c7ea3b4acef2744c282692bdb3c502e47c0ae23
arm7 <armprocessors7@gmail.com>
2026-01-31 11:31:10 -0500

    Refactor cache_top module and update bus connections

--- cache_top.svh
+++ cache_top.svh
@@ -1,20 +1,19 @@
-// typedef enum logic[1:0] { Invalid = 2'b00, Shared = 2'b01, Exclusive = 2'b10, Modified = 2'b11  } cache_state;
-
-// typedef enum logic [1:0] { No_OP = 2'b00, BusRd = 2'b01, BusRdX = 2'b10, BusUpgr = 2'b11 } bus_request;
-`include "mesi_types.svh"
+`include "mesi_types.sv"
 import mesi_types::*;
 
 module cache_top(
-    input logic clk = 1'b0, rst,
+    input logic clk, rst,
     input logic cpu_read[2], cpu_write[2],
     input logic [7:0] cpu_addr[2],
     input logic exclusive_1, exclusive_0
+
 );
 
-bus_request bus_cmd_in[2];
-bus_request bus_cmd_out;
-logic [7:0] bus_addr_in[2];
-logic [7:0] bus_addr_out;
+bus_request bus_cmd_in[2]; // from caches to Bus
+bus_request bus_cmd_out; // from Bus to caches
+logic [7:0] bus_addr_in[2]; // from caches to Bus
+logic [7:0] bus_addr_out; // from Bus to caches
+logic [1:0] bus_owner; // from Bus to Caches
 
 //logic [7:0] addr_out;
 
@@ -28,6 +27,7 @@ cache_mem cache_0(
     .bus_cmd_in(bus_cmd_out), 
     .address(cpu_addr[0]),
     .bus_addr_in(bus_addr_out),
+    .bus_owner(bus_owner[0]),
 
     .data_out(data_out[0]),
     .bus_cmd_out(bus_cmd_in[0]),
@@ -40,6 +40,7 @@ cache_mem cache_1(
     .bus_cmd_in(bus_cmd_out), 
     .address(cpu_addr[1]),
     .bus_addr_in(bus_addr_out),
+    .bus_owner(bus_owner[1]),
 
     .data_out(data_out[1]),
     .bus_cmd_out(bus_cmd_in[1]),
@@ -51,9 +52,12 @@ bus shared_bus(
     .clk(clk), .rst(rst),
     .cmd_in(bus_cmd_in),
     .bus_addr(bus_addr_in),
+    
 
     .cmd_out(bus_cmd_out),
-    .addr_out(bus_addr_out)
+    .addr_out(bus_addr_out),
+    .bus_owner(bus_owner)
 );
+
 
 endmodule
